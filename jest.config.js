cc8e283270f5a545469ab02ce49421623c44d845
PopovaOlha <fc.odess.10@gmail.com>
2023-11-12 16:32:35 +0200

    fix: change jest to vitest~

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-  module.exports = {
-    testEnvironment: 'jest-environment-jsdom',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    transform: {
-      '^.+\\.(ts|tsx)$': 'ts-jest',
-    },
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-    setupFilesAfterEnv: ['@testing-library/jest-dom'],
-  };
