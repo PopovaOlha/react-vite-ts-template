9dee573885e06af973ee09e45055586a744602ac
PopovaOlha <fc.odessa.10@gmail.com>
2023-11-22 18:25:11 +0300

    fix: Details.test.tsx

--- src/components/Details/Details.test.tsx
+++ src/components/Details/Details.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { AppStateProvider } from 'components/AppStateContext/AppStateContext';
 import Details from '../Details/Details';
 import data from 'assets/data/DataItems';
 
@@ -10,11 +9,7 @@ console.log(dataForTest);
 
 describe('<Details />', () => {
   it('renders card', () => {
-    render(
-      <AppStateProvider>
-        <Details />
-      </AppStateProvider>
-    );
+    render(<Details />);
 
     expect(screen.getByRole('img')).toBeInTheDocument();
     expect(screen.getAllByText(/hut/i)).not.toHaveLength(0);
